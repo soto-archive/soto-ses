37094cecaeb415c52776cbd62e1dfdf5fcb95d02
Adam Fowler <adamfowler71@gmail.com>
2020-06-02 16:59:41 +0100

    Version 4.7.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
 // swift-tools-version:5.0
-
 import PackageDescription
 
 let package = Package(
-  name: "SES",
-  products: [
-      .library(name: "SES", targets: ["SES"]),
-  ],
-  dependencies: [
-      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.0"))
-  ],
-  targets: [
-      .target(name: "SES", dependencies: ["AWSSDKSwiftCore"]),
-  ]
+    name: "SES",
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
+    products: [
+        .library(name: "SES", targets: ["SES"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.6.0"))
+    ],
+    targets: [
+        .target(name: "SES", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/SES"),
+    ]
 )
